f122a0fe901310f3f2107cf2d4da55a59d31275d
jimmyahalpara <jimmyahalpara123@gmail.com>
2022-01-15 00:00:07 +0530

    Update: Added include statements to include models which current model table has foreign key references of.

--- Model/Service/ServiceModel.php
+++ Model/Service/ServiceModel.php
@@ -2,6 +2,11 @@
     // prevent direct access to this file 
     include __DIR__ . '/../../Utilities/preventDirectAccess.php';
 
+    // include OrganizationModel.php 
+    include __DIR__ . '/../Organization/OrganizationModel.php';
+
+    // include PriceTypeModel.php
+    include __DIR__ . '/../PriceType/PriceTypeModel.php';
 
     class ServiceModel {
         // database connection
@@ -78,6 +83,136 @@
 
             return false;
         }
+
+
+        // function for updating service
+        public function update(){
+            // query to update record
+            $query = "UPDATE " . $this->table_name . "
+                        SET service_name = :service_name, description = :description, price_type_id = :price_type_id, price = :price, city = :city, organization_id = :organization_id, category_id = :category_id
+                        WHERE id = :id";
+
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+
+            // sanitize
+            $this->service_name=htmlspecialchars(strip_tags($this->service_name));
+            $this->description=htmlspecialchars(strip_tags($this->description));
+            $this->price_type_id=htmlspecialchars(strip_tags($this->price_type_id));
+            $this->price=htmlspecialchars(strip_tags($this->price));
+            $this->city=htmlspecialchars(strip_tags($this->city));
+            $this->organization_id=htmlspecialchars(strip_tags($this->organization_id));
+            $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+            $this->id=htmlspecialchars(strip_tags($this->id));
+
+            // bind values
+            $stmt->bindParam(":service_name", $this->service_name);
+            $stmt->bindParam(":description", $this->description);
+            $stmt->bindParam(":price_type_id", $this->price_type_id);
+            $stmt->bindParam(":price", $this->price);
+            $stmt->bindParam(":city", $this->city);
+            $stmt->bindParam(":organization_id", $this->organization_id);
+            $stmt->bindParam(":category_id", $this->category_id);
+            $stmt->bindParam(":id", $this->id);
+
+            // execute query
+            if($stmt->execute()){
+                return true;
+            }
+
+            return false;
+        }
+
+
+        // function for deleting service
+        public function delete(){
+            // query to delete
+            $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+
+            // bind data 
+            $stmt->bindParam(1, $this->id);
+
+            // execute query
+            if($stmt->execute()){
+                return true;
+            }
+
+            return false;
+
+        }
+
+
+        // function for reading single service
+        public function readOne(){
+            // query to read single record
+            $query = "SELECT * FROM " . $this->table_name . " WHERE id = ?";
+
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+
+            // bind parameters 
+            $stmt->bindParam(1, $this->id);
+
+            // execute query
+            $stmt->execute();
+
+            // get retrieved row
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // set values to object properties
+            $this->service_name = $row['service_name'];
+            $this->description = $row['description'];
+            $this->price_type_id = $row['price_type_id'];
+            $this->price = $row['price'];
+            $this->city = $row['city'];
+            $this->organization_id = $row['organization_id'];
+            $this->category_id = $row['category_id'];
+        }
+
+        // function to check if id is present or not
+        function isIdPresent(){
+            // query to read single record
+            $query = "SELECT * FROM " . $this->table_name . " WHERE id = ?";
+
+            // prepare query statement
+            $stmt = $this->conn->prepare($query);
+
+            // bind parameters 
+            $stmt->bindParam(1, $this->id);
+
+            // execute query
+            $stmt->execute();
+
+            // get retrieved row
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // set values to object properties
+            if ($row) {
+                return true;
+            }
+            return false;
+        }
+
+        
+        // get OrganizationModel object by id
+        function getOrganizationModelById(){
+            $organization_model = new OrganizationModel($this->conn);
+            $organization_model->id = $this->organization_id;
+            $organization_model->readOne();
+            return $organization_model;
+        }
+
+        
+        // get PriceType for given service
+        function getPriceType(){
+            $price_type_model = new PriceTypeModel($this->conn);
+            return $price_type_model->getTypeFromId($this->price_type_id);
+        }
+
+        
 
     }
 
