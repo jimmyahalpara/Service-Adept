ba60dcffa28de80649d6e9bdf389f751bb147bef
jimmyahalpara <jimmyahalpara123@gmail.com>
2022-01-16 01:08:15 +0530

    Feat: Created File to logout, which destroys all the data from the session

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();
+    // unset all session variables
+    session_unset();
+    // destroy the session
+    session_destroy();
+
+    header("Location: " . dirname($_SERVER['PHP_SELF']). "/userDetail.php");
+
+?>
