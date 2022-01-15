b12d7922c2c9a671eb736a8567bff3e7ec37c6b0
jimmyahalpara <jimmyahalpara123@gmail.com>
2022-01-15 19:04:17 +0530

    Fix: Removed unnecessary echo statements

--- createUser.php
+++ createUser.php
@@ -7,10 +7,7 @@
     require_once __DIR__ . "/Utilities/importAllModals.php";
     // require database connection
     require_once __DIR__ . "/database/configuration.php";
-    echo "<pre>";
-    // var_dump($_POST);
-    // var_dump($_POST);
-
+    
     function setUserCreationError($message){
         var_dump("TOOO");
         $_SESSION['createUserError'] = $message;
@@ -79,7 +76,6 @@
     }
     
 
-    echo "</pre>"
 
 ?>
 
