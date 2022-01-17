c51b1649848064e7f9d68fe8acc2ad4fe43ac92a
jimmyahalpara <jimmyahalpara123@gmail.com>
2022-01-17 23:49:55 +0530

    Fix: removed link line reffering to unavailable css file

--- Views/Header/header.php
+++ Views/Header/header.php
@@ -22,7 +22,6 @@
 
   <!-- Font Awesome icons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="../login/signup-style.css">
 
 </head>
 
